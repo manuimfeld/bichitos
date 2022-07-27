9e8d6cdd6333a12cd2bec8b80a14eee35e998890
Manuimfeld <manu_jadir@hotmail.com>
2022-07-27 15:14:27 -0300

    V1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CarritoImage from '../img/carrito-de-compras.svg'
+import BusquedaImage from '../img/busqueda.svg'
+import BurgerMenuImage from '../img/burguer-menu.svg'
+import BurgerMenu from './BurgerMenu'
 
 const Header = () => {
+
+  const [displayMenu, setDisplayMenu] = useState(false)
+
+  const handleClick = () => {
+    (displayMenu === false) ? setDisplayMenu(true) : setDisplayMenu(false)
+  }
+
   return (
+    <>
     <header>
       <nav>
         <ul>
-          <li>Menu</li>
-          <li>Bichitos</li>
+          <li onClick={handleClick}>
+            <img src={BurgerMenuImage} alt="" className='header-button'/>
+          </li>
+          <li>
+            <img src={BusquedaImage} alt="" className='header-button'/>
+          </li>
+          <li>
+            <img src={CarritoImage} alt="" className='header-button'/>
+          </li>
         </ul>
       </nav>
     </header>
+    { (displayMenu === false) ? (
+        null
+      )
+      : <BurgerMenu />
+    }
+    </>
   )
 }
 
