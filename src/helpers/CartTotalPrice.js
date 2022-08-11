a97f05b6d5e80d866277f9d7b4820940ee2175cd
Manuimfeld <manu_jadir@hotmail.com>
2022-08-10 23:37:30 -0300

    Cart total price

--- src/helpers/CartTotalPrice.js
+++ src/helpers/CartTotalPrice.js
@@ -1,12 +1,17 @@
 const CartTotalPrice = (cart) => {
-  let totalPrice = 0;
+  return cart.reduce(
+    (prev, curr) => prev + curr.item.attributes.Precio * curr.cantidad,
+    0
+  );
+
+  /* let totalPrice = 0;
 
   for (let i = 0; i < cart.length; i++) {
     let number = 0;
     number += cart[i].item.attributes.Precio * cart[i].cantidad;
     totalPrice += number;
   }
-  return totalPrice;
+  return totalPrice; */
 };
 
 export default CartTotalPrice;
