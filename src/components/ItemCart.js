f112c7d944eb6c41956d6c05d84f6485d651424c
Manuimfeld <manu_jadir@hotmail.com>
2022-10-19 11:36:29 -0300

    Add ItemCart component

---
+++ src/components/ItemCart.js
@@ -0,0 +1,36 @@
+import React from "react";
+import editItemAmount from "../helpers/editItemAmount";
+import removeItemCart from "../helpers/removeItemCart";
+
+const ItemCart = (product, index, cart, setCart) => {
+  return (
+    <article>
+      <img
+        src={`http://192.168.0.70:1337${product.attributes.Imagen.data.attributes.url}`}
+        alt=""
+      />
+      <div className="cart-product-info">
+        <p>Top nutrition</p>
+        <p>{product.attributes.Precio * product.cantidad}</p>
+        <div className="cart-product-count">
+          <p>Cantidad</p>
+          <button onClick={(e) => editItemAmount(e, cart, setCart, index)}>
+            -
+          </button>
+          <p>{product.cantidad}</p>
+          <button onClick={(e) => editItemAmount(e, cart, setCart, index)}>
+            +
+          </button>
+        </div>
+      </div>
+      <button
+        className="button-remove-product"
+        onClick={() => removeItemCart(product, cart, setCart)}
+      >
+        X
+      </button>
+    </article>
+  );
+};
+
+export default ItemCart;
