6466f41cfdf7f23113f5f26d642da3065f5c713a
Manuimfeld <manu_jadir@hotmail.com>
2022-10-22 15:03:08 -0300

    Edit FiltersMenu component

--- src/components/FiltersMenu.js
+++ src/components/FiltersMenu.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import FiltersModal from "./FiltersModal";
+import addFilters from "../helpers/addFilters";
 
 const FiltersMenu = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [filters, setFilters] = useState([]);
+
+  const handleClick = () => {
+    setShowModal(showModal === false ? true : false);
+  };
+
   return (
     <div className="container-filters">
       <div className="filters-menu">
-        <p>Filtrar</p>
+        <button onClick={() => handleClick()}>Filtrar</button>
+        <FiltersModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          filters={filters}
+          setFilters={setFilters}
+        />
       </div>
       <div className="filters">
-        {/* <article className="filter">
-          <p>Royal Cannin</p>
-          <button>x</button>
-        </article> */}
+        {filters.map((filtername) => (
+          <article className="filter" key={filtername}>
+            <p>{filtername}</p>
+            <button onClick={() => addFilters(filtername, filters, setFilters)}>
+              x
+            </button>
+          </article>
+        ))}
       </div>
     </div>
   );
