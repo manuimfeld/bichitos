af0a3ed2b013a69540afea68634c11da029fbdf3
Manuimfeld <manu_jadir@hotmail.com>
2022-10-22 15:03:54 -0300

    Add filters for map this

---
+++ src/helpers/filters.js
@@ -0,0 +1,6 @@
+const PRODUCT_TYPE = ["Alimento", "Higiene", "Juguetes y snacks"];
+const PET_TYPE = ["Perro", "Gato"];
+const PET_AGE = ["Cachorro", "Adulto"];
+const PET_SIZE = ["Pequeña", "Mediana", "Grande"];
+
+export { PRODUCT_TYPE, PET_TYPE, PET_AGE, PET_SIZE };
