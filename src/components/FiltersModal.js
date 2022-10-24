68961c9f4fea35990f5638b21530cdb3d361b8be
Manuimfeld <manu_jadir@hotmail.com>
2022-10-24 13:39:48 -0300

    Add feature select filters browse

--- src/components/FiltersModal.js
+++ src/components/FiltersModal.js
@@ -20,10 +20,11 @@ const FiltersModal = ({ showModal, setShowModal, filters, setFilters }) => {
         <>
           {PRODUCT_TYPE.map((type) => (
             <button
-              onClick={(e) =>
-                addFilters(e.target.innerText, filters, setFilters)
+              key={type}
+              onClick={(e) => addFilters("Tipo", type, filters, setFilters)}
+              className={
+                filters["Tipo"]?.includes(type) ? "filter-selected" : null
               }
-              className={filters.includes(type) ? "filter-selected" : null}
             >
               {type}
             </button>
@@ -34,8 +35,11 @@ const FiltersModal = ({ showModal, setShowModal, filters, setFilters }) => {
         <p>Mascota</p>
         {PET_TYPE.map((type) => (
           <button
-            onClick={(e) => addFilters(e.target.innerText, filters, setFilters)}
-            className={filters.includes(type) ? "filter-selected" : null}
+            key={type}
+            onClick={(e) => addFilters("Animal", type, filters, setFilters)}
+            className={
+              filters["Animal"]?.includes(type) ? "filter-selected" : null
+            }
           >
             {type}
           </button>
@@ -45,8 +49,11 @@ const FiltersModal = ({ showModal, setShowModal, filters, setFilters }) => {
         <p>Edad</p>
         {PET_AGE.map((age) => (
           <button
-            onClick={(e) => addFilters(e.target.innerText, filters, setFilters)}
-            className={filters.includes(age) ? "filter-selected" : null}
+            key={age}
+            onClick={(e) => addFilters("Edad", age, filters, setFilters)}
+            className={
+              filters["Edad"]?.includes(age) ? "filter-selected" : null
+            }
           >
             {age}
           </button>
@@ -56,8 +63,11 @@ const FiltersModal = ({ showModal, setShowModal, filters, setFilters }) => {
         <p>Raza (perro)</p>
         {PET_SIZE.map((size) => (
           <button
-            onClick={(e) => addFilters(e.target.innerText, filters, setFilters)}
-            className={filters.includes(size) ? "filter-selected" : null}
+            key={size}
+            onClick={(e) => addFilters("Raza", size, filters, setFilters)}
+            className={
+              filters["Raza"]?.includes(size) ? "filter-selected" : null
+            }
           >
             {size}
           </button>
